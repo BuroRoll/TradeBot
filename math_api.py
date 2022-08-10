4d1bf69c56255e8d727f082d87b7dbf6f4348f4e
Danil Konkov <BuroRoll@gmail.com>
2022-08-10 22:30:00 +0500

    add uptrend condition

--- math_api.py
+++ math_api.py
@@ -12,3 +12,10 @@ def get_trend_coef(ticket_data, deg):
     x = list(range(len(ticket_data)))
     p = np.poly1d(np.polyfit(x, ticket_data, deg))
     return p.coefficients[0]
+
+
+def get_is_uptrend(ticket_data, deg):
+    x = list(range(len(ticket_data)))
+    p = np.poly1d(np.polyfit(x, ticket_data, deg))
+    data = p(x)
+    return data[-1] > data[-2]
