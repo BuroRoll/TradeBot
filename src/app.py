507173742153431ace5d9edc0a4dfd5ed669a819
danilkonkov <buroroll@gmail.com>
2023-02-19 18:19:49 +0500

    SuperTrend вынесен в отдельный класс

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ import schedule
 
 from tools.binance_api import *
 from tools.telegram_bot_api import *
-from tools.super_trend import get_status
+from tools.super_trend import SuperTrend
 from settings import settings
 
 api_key = settings.BINANCE_API_KEY
@@ -20,11 +20,13 @@ client = Spot(key=api_key, secret=secret_key)
 
 
 def trading():
+    logger.info(f'Начало торговли')
     last_order = client.get_orders(trade_pair, limit=1)[0]['side']
     is_buy = True if last_order == 'BUY' else False
     logger.info(f'Последний статус: {last_order}')
-    status = get_status()
-    logger.info(f'Статус супертренда: {status.status}')
+    super_trend = SuperTrend()
+    status: str = super_trend.get_status()
+    logger.info(f'Статус супертренда: {status}')
     current_price = get_current_ticket_price(client, trade_pair)
     logger.info(f'Текущая цена актива {trade_pair}: {current_price}')
     # data = get_ticket_time_data(client, trade_pair, ticket_time, 20)
@@ -32,7 +34,7 @@ def trading():
     # is_uptrend = get_is_uptrend(data, 3)
     # Проверка на то, что цена выше тренда и тренд восходящий
     # if current_price >= trend_price and is_uptrend and not is_buy:
-    if status.status == 'up' and not is_buy:
+    if status == 'up' and not is_buy:
         usdt_balance = get_balance(client, 'USDT')
         buy_count = usdt_balance / current_price - 0.0001
         formated_buy_count = float("{0:.4f}".format(buy_count))
@@ -40,7 +42,7 @@ def trading():
         log_string = f'Покупка с ценой: {current_price}, количество: {formated_buy_count}'
         logger.info(log_string)
         send_notification_to_telegram(log_string)
-    elif status.status == 'down' and is_buy:
+    elif status == 'down' and is_buy:
         eth_balance = get_balance(client, 'ETH')
         formated_sell_count = '{0:.4f}'.format(float("{0:.4f}".format(eth_balance)) - 0.0001)
         binance_trade(client, trade_pair, 'SELL', formated_sell_count)
